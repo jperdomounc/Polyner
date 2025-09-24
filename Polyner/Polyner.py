dae15780adfd4bd886e9b408e6ff912b4dc7418d
jperdomounc <jcperdomo100@gmail.com>
2025-09-24 17:53:43 -0400

    2d fixes

--- Polyner/Polyner.py
+++ Polyner/Polyner.py
@@ -29,11 +29,6 @@ def train(img_id, config):
     proj_pos_path = '{}/fanSensorPos.nii'.format(in_path)
     mask_path = '{}/mask_{}.nii'.format(in_path, img_id)
     h, w, SOD = config["file"]["h"], config["file"]["w"], config["file"]["SOD"]
-    d = 1  # Set to 1 for 2D fan beam
-    SDD = config["file"]["SDD"]
-    detector_h, detector_w = config["file"]["detector_h"], config["file"]["detector_w"]
-    cone_angle = config["file"]["cone_angle"]
-    n_projections = config["file"]["n_projections"]
     voxel_size = config["file"]["voxel_size"]
     num_angle, _ = sitk.GetArrayFromImage(sitk.ReadImage(proj_path)).shape
 
@@ -51,15 +46,11 @@ def train(img_id, config):
 
     device = torch.device('cuda:{}'.format(str(gpu) if torch.cuda.is_available() else 'cpu'))
 
-    # mask - extend to 3D
+    # mask
     # -----------------------
     mask = sitk.GetArrayFromImage(sitk.ReadImage(mask_path))
-    if len(mask.shape) == 2:
-        # Extend 2D mask to 3D
-        mask = np.repeat(mask[:, :, np.newaxis], d, axis=2)
-    mask = np.pad(mask, ((int(SOD - (mask.shape[0] / 2)), int(SOD - (mask.shape[0] / 2))-1),
-                         (int(SOD - (mask.shape[1] / 2)), int(SOD - (mask.shape[1] / 2))-1),
-                         (int(SOD - (mask.shape[2] / 2)), int(SOD - (mask.shape[2] / 2))-1))).copy()
+    mask = np.rot90(np.pad(mask, ((int(SOD - (mask.shape[0] / 2)), int(SOD - (mask.shape[0] / 2))-1),
+                                  (int(SOD - (mask.shape[1] / 2)), int(SOD - (mask.shape[1] / 2))-1)))).copy()
     mask = torch.tensor(mask).float().unsqueeze(0).unsqueeze(0).to(device)
     mask = torch.where(mask == 1, 0., 1.)
 
@@ -78,7 +69,7 @@ def train(img_id, config):
     dc_loss = torch.nn.L1Loss().to(device)
     ase_loss = model.Attenuation_Smootion_Over_Energies_Loss(lamb=lamb, mask=mask).to(device)
 
-    network = tcnn.NetworkWithInputEncoding(n_input_dims=3, n_output_dims=e_level,
+    network = tcnn.NetworkWithInputEncoding(n_input_dims=2, n_output_dims=e_level,
                                             encoding_config=config["encoding"], network_config=config["network"]).to(device)
     optimizer = torch.optim.Adam(params=network.parameters(), lr=lr)
     scheduler = lr_scheduler.StepLR(optimizer, step_size=lr_decay_epoch, gamma=lr_decay_coefficient)
@@ -86,12 +77,11 @@ def train(img_id, config):
     # data loader
     # -----------------------
     train_loader = data.DataLoader(
-        dataset=dataset.TrainData3D(proj_path=proj_path, proj_pos_path=proj_pos_path, SOD=SOD, SDD=SDD,
-                                    detector_h=detector_h, detector_w=detector_w, cone_angle=cone_angle,
-                                    num_sample_ray=num_sample_ray, num_angle=num_angle, voxel_size=voxel_size),
-                                    batch_size=batch_size, shuffle=True)
+        dataset=dataset.TrainData(proj_path=proj_path, proj_pos_path=proj_pos_path, SOD=SOD,
+                                  num_sample_ray=num_sample_ray, num_angle=num_angle, voxel_size=voxel_size),
+                                  batch_size=batch_size, shuffle=True)
     test_loader = data.DataLoader(
-        dataset=dataset.TestData3D(h=(2 * SOD) + 1, w=(2 * SOD) + 1, d=(2 * SOD) + 1), batch_size=1, shuffle=False)
+        dataset=dataset.TestData(h=(2 * SOD) + 1, w=(2 * SOD) + 1), batch_size=1, shuffle=False)
 
     # optimization & reconstruction
     # -----------------------
@@ -101,15 +91,14 @@ def train(img_id, config):
         network.train()
         loss_log = 0
         for i, (ray, proj) in enumerate(train_loader):
-            ray = ray.to(device).float().view(-1, 3)   # (batch_size*num_sample_ray*ray_length, 3)
+            ray = ray.to(device).float().view(-1, 2)   # (batch_size*num_sample_ray*2*SOD, 2)
             proj = proj.to(device).float()  # (batch_size, num_sample_ray)
-            # (batch_size*num_sample_ray*ray_length, e_level)
-            ray_length = ray.shape[0] // (ray.shape[0] // (num_sample_ray * batch_size))
-            intensity_pre = network(ray).view(-1, num_sample_ray, ray_length // num_sample_ray, e_level).float()
-            # forward model - 3D cone beam projection
+            # (batch_size*num_sample_ray*2*SOD, e_level)
+            intensity_pre = network(ray).view(-1, num_sample_ray, 2 * SOD, e_level).float()
+            # forward model
             proj_pre = torch.exp(-voxel_size *
-                                 torch.sum(intensity_pre, dim=2))  # (batch_size, num_sample_ray, e_level)
-            proj_pre = -torch.log(torch.sum(proj_pre * spectrum, dim=-1))  # (batch_size, num_sample_ray)
+                                 torch.sum(intensity_pre, dim=2).squeeze(-1))  # (batch_size, num_sample_ray, e_level)
+            proj_pre = -torch.log(torch.sum(proj_pre * spectrum, dim=-1).squeeze(-1))  # (batch_size, num_sample_ray)
             # calculate loss
             loss = dc_loss(proj_pre, proj.to(proj_pre.dtype)) + ase_loss(intensity=intensity_pre, ray=ray)
 
@@ -125,17 +114,17 @@ def train(img_id, config):
         # model save & reconstruction
         if (e + 1) % save_epoch == 0:
             img_all = []
-            kx, ky, kz = int(1 + ((2 * SOD) - h)/2), int(((2 * SOD) - w)/2), int(((2 * SOD) - d)/2)
+            kx, ky = int(1 + ((2 * SOD) - h)/2), int(((2 * SOD) - w)/2)
             final_loss = loss_log / len(train_loader)
             # Calculate iterations per second
             elapsed_time = time.time() - epoch_start_time
             iterations_per_sec = (e + 1) / elapsed_time if elapsed_time > 0 else 0
             with torch.no_grad():
                 torch.save(network.state_dict(), '{}/model_{}.pkl'.format(model_path, img_id))
-                for i, (xyz) in enumerate(test_loader):
-                    xyz = xyz.to(device).float().view(-1, 3)  # (h*w*d, 3)
-                    img_pre = network(xyz)[:, int(np.mean(np.arange(0, e_level)))].view((2 * SOD) + 1, (2 * SOD) + 1, (2 * SOD) + 1)
-                    img_pre = img_pre.float().cpu().detach().numpy()[kx:kx + h, ky:ky + w, kz:kz + d]
+                for i, (xy) in enumerate(test_loader):
+                    xy = xy.to(device).float().view(-1, 2)  # (h*w, 2)
+                    img_pre = network(xy)[:, int(np.mean(np.arange(0, e_level)))].view((2 * SOD) + 1, (2 * SOD) + 1)
+                    img_pre = img_pre.float().cpu().detach().numpy()[kx:kx + h, ky:ky + w]
                     img_pre = np.flip(img_pre, axis=1)
 
                 sitk.WriteImage(sitk.GetImageFromArray(img_pre), '{}/polyner_{}_{:.5f}_{:.2f}it_s.nii'.format(out_path, img_id, final_loss, iterations_per_sec))
