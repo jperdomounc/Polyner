bd741fb5ec6c49a34e1546f18d79794026ebbab9
jperdomounc <jcperdomo100@gmail.com>
2026-01-29 13:06:54 -0500

    10 kev spectrum fixes for key error

--- Polyner/test.py
+++ Polyner/test.py
@@ -88,7 +88,7 @@ def train(img_id, config):
 
     # energy spectrum
     # -----------------------
-    spectrum = scio.loadmat('./{}/DECBCTSpectrum110KVP.mat'.format(in_path))['DECBCTSpectrum110KVP']
+    spectrum = scio.loadmat('./{}/DECBCTSpectrum110KVP.mat'.format(in_path))['DECBCTSpectrum110KVP_bin']
 
     e_1, e_n = 1, 11
     spectrum = spectrum[e_1-1:e_n, 0] # 0->LE, 1->HE
