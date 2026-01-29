219bc8cea269cec08e83e9c20f58ba3204d4ed88
jperdomounc <jcperdomo100@gmail.com>
2026-01-29 12:39:21 -0500

    10 kev spectrum

--- Polyner/Polyner.py
+++ Polyner/Polyner.py
@@ -59,7 +59,7 @@ def train(img_id, config):
     # -----------------------
     spectrum = scio.loadmat('./{}/DECBCTSpectrum110KVP.mat'.format(in_path))['DECBCTSpectrum110KVP']
 
-    e_1, e_n = 20, 110
+    e_1, e_n = 1, 11
     spectrum = spectrum[e_1-1:e_n, 0] # 0->LE, 1->HE
     spectrum = spectrum / np.sum(spectrum)
     e_level = len(spectrum)
